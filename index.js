4aa2a62f4bc6843c49d183b6d8ee7123fb48d5b5
Bryan Clark <clarkbw@gmail.com>
2026-06-30 18:02:24 -0700

    feat: import geo, snapshot, and EventStore from signalk-dsc

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
-module.exports = {};
+const geo = require('./lib/geo');
+const snapshot = require('./lib/snapshot');
+const { EventStore } = require('./lib/store');
+
+module.exports = { ...geo, ...snapshot, EventStore };
